97359312264ed2777e51456960c4a7a2259b7502
RamonaEJSpence <ramonaejspence@gmail.com>
2020-01-30 10:54:09 -0500

    added boiler plates for constructors and subClasses

--- script/main.js
+++ script/main.js
@@ -1,1 +1,49 @@
 console.log("hello");
+
+
+class Enemy {
+  constructor(name) {
+
+  }
+}
+
+let JavaScript = new Enemy {
+  constructor(name) {
+    super();
+  }
+}
+
+let Css = new Enemy {
+  constructor(name) {
+    super();
+  }
+}
+
+let BoaConstructor = new Enemy {
+  constructor(name) {
+    super();
+  }
+}
+
+class Player {
+  constructor(name) {
+
+  }
+}
+
+let Rachel = new Player {
+  constructor(name) {
+    super();
+  }
+}
+
+let Marcos = new Player {
+  constructor();
+  super();
+}
+
+let Ramona = new Player {
+  constructor() {
+  super();
+  }
+}
